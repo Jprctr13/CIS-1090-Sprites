f8d3a7523ddd89cd9ab683e7ef141a352b9e1881
jcp05131 <jcp05131@castleton.edu>
2022-12-09 10:47:14 -0500

    Worked on Game

--- src/games/homework.js
+++ src/games/homework.js
@@ -4,7 +4,7 @@ let score;  //The players score
 let alive;  //is the 
 
 //You might have some constants that you use
-const speed = 300;  //In pixels per second
+//In pixels per second
 
 //This is a helper function to compute the distance
 //between two sprites
@@ -23,18 +23,15 @@ function setup(sprites) {
     //But you can use emojis!
     // https://emojis.wiki/
 
-    sprites[0].image = "🧍‍♂️"; //A fire engine
-    sprites[0].x = 100;
-    sprites[0].y = 100;
 
     //Putting two sprites together you
     //can make more complicated things.
-    sprites[1].image = "🏠"; //A fire engine
+    sprites[1].image = "🧤"; //A fire engine
     sprites[1].x = 300;
     sprites[1].y = 100;
-    sprites[2].image = "🔥"; //A fire engine
+    sprites[2].image = "⚾"; //A fire engine
     sprites[2].x = 300;
-    sprites[2].y = 120;
+    sprites[2].y = 500;
 
 }
 
@@ -50,43 +47,38 @@ function setup(sprites) {
  * @param space     Is spacebar pressed?
  * @returns The current score
  */
-function frame(sprites, t, dt, up, down, left, right, space) {
-    //Keep references to the sprites in some variables with
-    //better names:
-    const truck = sprites[0]; //Easier to remember
-    const house = sprites[1]; //Easier to remember
-    const fire = sprites[2]; //Easier to remember
 
-    //Move the fire engine
-    if (up) {
-        //Speed is in pixels per second, and
-        //dt is the number of seconds that have
-        //passed since the last frame.
-        //
-        //Multiply them together so that the
-        //truck moves at the same speed if the
-        //computer is fast or slow
-        truck.y += speed * dt;
-    } 
-    if (down) {
-        truck.y -= speed * dt;
-    }
-    if (right) {
-        truck.x += speed * dt;
-        //You can flipH a spright so it is facing
-        //the other direction
-        truck.flipH = true;
-    }
-    if (left) {
-        truck.x -= speed * dt;
-        truck.flipH = false;
+let speed = 150;
+const gravity = 450;
+
+function frame(sprites, t, dt, up, down, left, right, space){ {
+
+    const ball = sprites[2];
+    
+    sprites[2].y = sprites[2].y - dt * speed;
+    
+    if (sprites[2].y <= 0){
+        ball.y = 450;
+        speed = 150;
+        ball.x = math.random() * 750;
     }
 
-    //If the truck is close to the house
-    if ( distance(truck, house) < 10 ){
-        fire.image = ""; //Make the fire go away
-    }
 
+    return 0;
+}
+//move glove
+if (up) {
+    glove.y += walk * dt;
+} if (down) {
+    glove.y -= walk * dt;
+}
+if (right) {
+    glove.x += walk * dt;
+}
+if (left) {
+    glove.x -= walk * dt;
+}
+   
     //A very simple repeating animation
     sprites[2].y += Math.sin(t)/10;
 
